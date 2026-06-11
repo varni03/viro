34df6492c52650bd8d4a1ee713212d73490845fd
varni03 <jainvarnika006@gmail.com>
2026-06-11 11:50:13 -0400

    Fix init call

--- database/db.py
+++ database/db.py
@@ -17,7 +17,7 @@ class ViroDB:
         else:
             self.conn = sqlite3.connect(DB_PATH, check_same_thread=False)
             self.conn.row_factory = sqlite3.Row
-        self._tables()
+        self.setup_tables()
 
     def get_engine(self):
         if not hasattr(self, '_engine'):
